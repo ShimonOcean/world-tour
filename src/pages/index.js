b40f75b5f2a4108755ee59a44b6eb622d475c968
ShimonIslam <shimon.ocean@gmail.com>
2021-09-14 14:47:27 -0700

    Fixed folder structure, main and footer renders

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout/Layout'
 
 export default function Home() {
   return <Layout>main</Layout>;
