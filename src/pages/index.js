deeefbf0b7f91c353b1406961a18931758d8db43
ShimonIslam <shimon.ocean@gmail.com>
2021-09-14 15:00:12 -0700

    Fetching API data successful, but runtime error

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,20 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout/Layout'
 
-export default function Home() {
-  return <Layout>main</Layout>;
+export default function Home({ countries }) {
+  console.log(countries);
+  return <Layout>
+    <div className={styles.counts}>Found {countries.length} countries</div>
+  </Layout>;
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+  
+  return {
+    props: {
+      countries,
+    }
+  }
 }
