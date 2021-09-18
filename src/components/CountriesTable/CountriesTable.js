8d611f65b466f11ca7fb36baa0ecd8646e00fe28
ShimonIslam <shimon.ocean@gmail.com>
2021-09-17 20:11:25 -0700

    Fixed orderedCountries, did not need to cast array on countries.sort

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -3,17 +3,39 @@ import styles from './CountriesTable.module.css';
 
 const orderBy = (countries, direction) => {
     if (direction === 'asc'){
-        return [countries].sort((a,b) => a.population > b.population ? 1 : -1)
+        return countries.sort((a,b) => a.population > b.population ? 1 : -1)
     }
     if (direction === 'desc'){
-        return [countries].sort((a,b) => a.population > b.population ? -1 : 1)
+        return countries.sort((a,b) => a.population > b.population ? -1 : 1)
     }
     return countries;
 }
 
+const SortArrow = ({ direction }) => {
+    if (!direction){
+        return <></>;
+    }
+
+    if (direction === 'desc'){
+        return (
+            <div className={styles.heading_arrow}>
+                <KeyboardArrowDownRounded/>
+            </div>  
+        )
+    } else {
+        return (
+            <div className={styles.heading_arrow}>
+                <KeyboardArrowUpRounded/>
+            </div>  
+        )
+    }
+}
+
 const CountriesTable = ({ countries }) => {
 
-    const orderedCountries = orderBy(countries, "desc");
+    const orderedCountries = orderBy(countries, 'desc');
+    console.log(orderedCountries);
+    console.log(countries)    
 
     return (
         <div>
@@ -22,9 +44,7 @@ const CountriesTable = ({ countries }) => {
                     <div>
                     Name 
                     </div>
-                    <div className={styles.heading_arrow}>
-                        <KeyboardArrowDownRounded/>
-                    </div>
+                    <SortArrow />
 
                 </button>
 
@@ -33,14 +53,12 @@ const CountriesTable = ({ countries }) => {
                     Population
                     </div>
 
-                    <div className={styles.heading_arrow}>
-                        <KeyboardArrowDownRounded/>
-                    </div>
+                    <SortArrow direction="desc"/>
                 </button>
 
             </div>
-
-            {countries.map((country) => (<div className={styles.row}>
+            
+            {orderedCountries.map((country) => (<div className={styles.row}>
                 <div className={styles.name} key={styles.name}>{country.name}</div>
 
                 <div className={styles.population} key={styles.population}>{country.population}</div>
