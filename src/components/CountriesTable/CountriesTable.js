f415fd4b0dac466b0ac8457049bb7b4b694fadc1
ShimonIslam <shimon.ocean@gmail.com>
2021-09-17 19:57:49 -0700

    Asc and Desc country listings working

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,12 +1,18 @@
 import styles from './CountriesTable.module.css';
 
-const orderBy = (countries) => {
-    return countries.sort((a,b) => a.population > b.population ? 1 : -1)
+const orderBy = (countries, direction) => {
+    if (direction === 'asc'){
+        return countries.sort((a,b) => a.population > b.population ? 1 : -1)
+    }
+    if (direction === 'desc'){
+        return countries.sort((a,b) => a.population > b.population ? -1 : 1)
+    }
+    return countries;
 }
 
 const CountriesTable = ({countries}) => {
 
-    const orderedCountries = orderBy(countries);
+    const orderedCountries = orderBy(countries, "desc");
 
     return <div>
         <div className={styles.heading}>
@@ -25,9 +31,9 @@ const CountriesTable = ({countries}) => {
         </div>
 
         {countries.map((country) => <div className={styles.row}>
-            <div className={styles.name}>{country.name}</div>
+            <div className={styles.name} key={styles.name}>{country.name}</div>
 
-            <div className={styles.population}>{country.population}</div>
+            <div className={styles.population} key={styles.population}>{country.population}</div>
         </div>)}
     </div>
 }
