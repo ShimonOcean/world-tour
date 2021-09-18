da73c1ba2148cb44fcb6195bf55dca07188b5cd3
ShimonIslam <shimon.ocean@gmail.com>
2021-09-17 20:02:29 -0700

    orderedCountries variable not rendering in map, needs fixing

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -2,40 +2,42 @@ import styles from './CountriesTable.module.css';
 
 const orderBy = (countries, direction) => {
     if (direction === 'asc'){
-        return countries.sort((a,b) => a.population > b.population ? 1 : -1)
+        return [countries].sort((a,b) => a.population > b.population ? 1 : -1)
     }
     if (direction === 'desc'){
-        return countries.sort((a,b) => a.population > b.population ? -1 : 1)
+        return [countries].sort((a,b) => a.population > b.population ? -1 : 1)
     }
     return countries;
 }
 
-const CountriesTable = ({countries}) => {
+const CountriesTable = ({ countries }) => {
 
     const orderedCountries = orderBy(countries, "desc");
 
-    return <div>
-        <div className={styles.heading}>
-            <button className={styles.heading_name}>
-                <div>
-                   Name 
-                </div>
-            </button>
+    return (
+        <div>
+            <div className={styles.heading}>
+                <button className={styles.heading_name}>
+                    <div>
+                    Name 
+                    </div>
+                </button>
 
-            <button className={styles.heading_population}>
-                <div>
-                   Population
-                </div>
-            </button>
+                <button className={styles.heading_population}>
+                    <div>
+                    Population
+                    </div>
+                </button>
 
+            </div>
+
+            {countries.map((country) => (<div className={styles.row}>
+                <div className={styles.name} key={styles.name}>{country.name}</div>
+
+                <div className={styles.population} key={styles.population}>{country.population}</div>
+            </div>))}
         </div>
-
-        {countries.map((country) => <div className={styles.row}>
-            <div className={styles.name} key={styles.name}>{country.name}</div>
-
-            <div className={styles.population} key={styles.population}>{country.population}</div>
-        </div>)}
-    </div>
+    )
 }
 
 export default CountriesTable;
