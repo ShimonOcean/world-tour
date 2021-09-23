e9442b032e7646e20ccce8712bcc20be575708d2
ShimonIslam <shimon.ocean@gmail.com>
2021-09-22 19:16:15 -0700

    request to API failed, at current commit link is down and not working

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,16 +1,21 @@
-import { KeyboardArrowDownRounded, KeyboardArrowUpOutlined } from '@material-ui/icons';
+import {
+    KeyboardArrowDownRounded,
+    KeyboardArrowUpRounded,
+  } from "@material-ui/icons";
 import { useState } from 'react';
 import styles from './CountriesTable.module.css';
 
-const orderBy = (countries, direction) => {
-    if (direction === 'asc'){
-        return countries.sort((a,b) => a.population > b.population ? 1 : -1)
+const orderBy = (countries, value, direction) => {
+    if (direction === "asc") {
+      return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
     }
-    if (direction === 'desc'){
-        return countries.sort((a,b) => a.population > b.population ? -1 : 1)
+  
+    if (direction === "desc") {
+      return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
     }
+  
     return countries;
-}
+  };
 
 const SortArrow = ({ direction }) => {
     if (!direction){
@@ -26,7 +31,7 @@ const SortArrow = ({ direction }) => {
     } else {
         return (
             <div className={styles.heading_arrow}>
-                <KeyboardArrowUpOutlinedUp color="inherit"/>
+                <KeyboardArrowUpRounded color="inherit"/>
             </div>  
         )
     }
@@ -36,7 +41,7 @@ const CountriesTable = ({ countries }) => {
     const [direction, setDirection] = useState();
     const [value, setValue] = useState();
 
-    const orderedCountries = orderBy(countries, 'desc');  
+    const orderedCountries = orderBy(countries, direction);  
 
     const switchDirection = () => {
         if (!direction){
